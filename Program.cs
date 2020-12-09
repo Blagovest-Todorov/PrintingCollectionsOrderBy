dc0b43922a53cb6b438fdfd8b9f7ee63925fb1d6
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-09 10:24:55 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _04.SortingCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            numbers.Add(5);
+            numbers.Add(7);
+            numbers.Add(10);
+            numbers.Add(-5);
+
+
+            List<string> stringNumbers = new List<string>();
+            stringNumbers.Add("5");
+            stringNumbers.Add("7");
+            stringNumbers.Add("10");
+            stringNumbers.Add("-5");
+
+            Console.WriteLine(string.Join(" ", numbers.OrderBy(x=> x))); // printing them sorted ascending !
+
+            Console.WriteLine(string.Join(" ", stringNumbers.OrderBy(x=> x))); //different output than upper print
+        }
+    }
+}
